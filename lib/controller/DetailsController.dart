b1277e8a88b3a59b2702f2319e733c1bd426e8a9
elt <info@eltapp.com>
2025-10-18 18:24:55 +0530

    changes

---
+++ lib/controller/DetailsController.dart
@@ -0,0 +1,20 @@
+import 'package:finly/ui/HomeScreen.dart';
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class DetailsController extends GetxController{
+  DateTime? selectedDate;
+  var budgetAmount = 0.0.obs;
+  var totalAmount = 0.0.obs;
+
+  Future<void> loadTotalAmount() async{
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    totalAmount.value = sharedPreferences.getDouble('totalAmount') ?? 0.0;
+    budgetAmount.value = sharedPreferences.getDouble('budget') ?? 0.0;
+  }
+
+  @override
+  void onInit() {
+    loadTotalAmount();
+  }
+}
--- lib/controller/detailsConotroller.dart
+++
@@ -1,7 +0,0 @@
-import 'package:get/get.dart';
-
-class DetailsController extends GetxController{
-  DateTime? selectedDate;
-  var budgetAmount = 0.0.obs;
-  var totalAmount = 0.0.obs;
-}
