2e759d12d93d7cfc5801919870507b4504e797f3
elt <info@eltapp.com>
2025-10-24 16:41:03 +0530

    changes

--- lib/model/Category.dart
+++ lib/model/Category.dart
@@ -1,6 +1,24 @@
-class Category{
+class Category {
   final String categoryName;
   final String totalAmount;
+  final String id;
 
-  Category({required this.categoryName, required this.totalAmount});
+  Category({
+    required this.categoryName,
+    required this.totalAmount,
+    required this.id,
+  });
+
+  //option : if saving data to json
+  Map<String, dynamic> toJson() => {
+        'categoryName': categoryName,
+        'totalAmount': totalAmount,
+        'id': id,
+      };
+
+  factory Category.fromJson(Map<String, dynamic> json) => Category(
+        id: json['id'],
+        categoryName: json['categoryName'],
+        totalAmount: json['totalAmount'],
+      );
 }
